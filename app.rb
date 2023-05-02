485f7e8354c391811b561a2760d43cb33b33cdb6
Caroline Evans <carolineevans261@gmail.com>
2023-05-02 15:21:41 +0100

    finished section 3 phase 2

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # Root path (homepage, index page)
+  get '/' do
+    return 'Hello world!'
+  end
+
+  get '/hello' do
+    name = params[:name]
+
+    "Hello, #{name}"
+  
+  end
+
+  post '/submit' do
+
+    name = params[:name]
+    message = params[:message]
+
+    return "Thanks #{name}, you sent this message: #{message}"
+  end
+
+end
